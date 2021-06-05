6f9ac9afbbe55d23968f377805c17fead45e1607
Allyogi84 <anthonyallgood84@gmail.com>
2021-06-05 21:11:14 +0600

    Superfist plugin js,css filed added

--- cleanarc_power/header.php
+++ cleanarc_power/header.php
@@ -21,6 +21,8 @@
 
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/media.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/libs/megafish.css">
+		
 </head>
 <?php wp_head(); ?>
 
